02a926c4558f075eff10d928f0b0b18512337bff
Ashwin Mathi <ashwinmathi@gmail.com>
2026-02-10 20:57:34 -0500

    Add flake.nix for dev environment setup

---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  description = "DataFrame symbolic regression";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          pkgs' = import inputs.nixpkgs {
+            inherit system;
+            config.allowBroken = true;
+          };
+
+          haskellPackages = pkgs'.haskellPackages.extend (
+            final: prev: {
+              # Use the correct hash that Nix gave us
+              random = pkgs'.haskell.lib.dontCheck (
+                prev.callHackageDirect {
+                  pkg = "random";
+                  ver = "1.3.0";
+                  sha256 = "sha256-AzXPz8oe+9lAS60nRNFiRTEDrFDB0FPLtbvJ9PB7brM=";
+                } { }
+              );
+
+              srtree = pkgs'.haskell.lib.dontCheck (
+                prev.callHackageDirect {
+                  pkg = "srtree";
+                  ver = "2.0.1.6";
+                  sha256 = "sha256-D561wnKoFRr/HSviacsbtF5VnJHehG73LOmWM4TxlNs="; # Nix will tell you
+                } { }
+              );
+
+              # Jailbreak packages that depend on old random
+              time-compat = pkgs'.haskell.lib.doJailbreak prev.time-compat;
+              splitmix = pkgs'.haskell.lib.doJailbreak prev.splitmix;
+
+              # Mark dataframe as unbroken
+              dataframe = pkgs'.haskell.lib.markUnbroken prev.dataframe;
+            }
+          );
+
+        in
+        {
+          packages = {
+            default = config.packages.symbolic-regression;
+            symbolic-regression = haskellPackages.callCabal2nix "symbolic-regression" ./. { };
+          };
+
+          devShells.default = haskellPackages.shellFor {
+            packages = p: [ config.packages.symbolic-regression ];
+            buildInputs = with pkgs'; [
+              cabal-install
+              haskell-language-server
+              git
+              fourmolu
+            ];
+          };
+        };
+    };
+}
